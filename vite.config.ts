373fffe6c1403e40950da446fa661ba66aa6c8ba
bozitoma <bozitoma@gmail.com>
2023-09-17 00:46:08 +0900

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// index.htmlのルートディレクトリを変更
+// 参考
+// https://zenn.dev/junseinagao/scraps/a4e6b1413ff26e
+// https://zenn.dev/s_takashi/articles/d033401905ccf9
+const root = resolve(__dirname, 'src')
+
+// ビルドの出力先をプロジェクトルート直下に変更
+const outDir = resolve(__dirname, '');
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  root,
+  plugins: [react()],
+  build: {
+    outDir,
+    rollupOptions: {
+      input: {
+        dashboard: resolve(root, "dashboard", 'index.html'),
+        graphics: resolve(root, "graphics", 'index.html')
+      }
+    }
+  },
+})
