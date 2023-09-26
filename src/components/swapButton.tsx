2940500cf340e72177d6d86c1d8bc08313ce8943
bozitoma <bozitoma@gmail.com>
2023-09-27 02:51:45 +0900

    ✨ スワップボタンコンポーネントの作成

---
+++ src/components/swapButton.tsx
@@ -0,0 +1,12 @@
+import IconButton from '@mui/material/IconButton';
+import SwapHorizontalCircleRoundedIcon from '@mui/icons-material/SwapHorizontalCircleRounded';
+
+export function SwapButtons() {
+  return (
+    <>
+      <IconButton color="primary">
+        <SwapHorizontalCircleRoundedIcon />
+      </IconButton>
+    </>
+  )
+}
