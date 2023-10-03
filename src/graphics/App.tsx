0bba9f7b080c3bd2c5bbafea4321fdb844f570e1
bozitoma <bozitoma@gmail.com>
2023-10-04 00:13:12 +0900

    ⚡️ グラフィックスにレプリカントを表示

--- src/graphics/App.tsx
+++ src/graphics/App.tsx
@@ -1,42 +1,13 @@
-// import * as React from "react";
-import { Autocomplete, TextField } from "@mui/material";
-
-// type Props = {
-//   text: string
-//   url: string
-// }
-
-const characterIcon = [
-  { label: 'マリオ', value: 'mario' },
-  { label: 'ルイージ', value: 'luigi' },
-  { label: 'ワリオ', value: 'wario' },
-  { label: 'ワルイージ', value: 'waluigi' },
-  { label: 'ピーチ', value: 'peach' },
-  { label: 'デイジー', value: 'daisy' },
-  { label: 'ベビィマリオ', value: 'babymario' },
-  { label: 'キノピオ', value: 'toad' },
-  { label: 'ヨッシー', value: 'yoshi' },
-  { label: 'キャサリン', value: 'birdo' },
-  { label: 'ドンキーコング', value: 'dk' },
-  { label: 'クッパ', value: 'bowser' },
-  { label: 'パタパタ', value: 'paratroopa' },
-  { label: 'テレサ', value: 'boo' },
-  { label: 'ドンキーコングJr.', value: 'dkjr' },
-  { label: 'ヘイホー', value: 'shyguy' }
-]
+import { useReplicant } from '../hooks/useReplicant';
 
 function App() {
+  const [round] = useReplicant('bestOfInfo');
+
   return (
     <>
-      <Autocomplete
-        disablePortal
-        id="combo-box-demo"
-        options={characterIcon}
-        sx={{ width: 300 }}
-        renderInput={(params) => <TextField {...params} label="Character" />}
-      />
+      <div>{round}</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
