e72c00bb81cce298d3cceda4fec5fd1e10b758fc
bozitoma <bozitoma@gmail.com>
2023-10-03 23:04:54 +0900

    ✨ useReplicantをカスタムフックで作成

---
+++ src/types/global.d.ts
@@ -0,0 +1,11 @@
+import type { CreateNodecgInstance } from 'ts-nodecg/browser';
+import type { ReplicantMap } from './replicant';
+
+declare global {
+  const nodecg: CreateNodecgInstance<
+    'mariotennis64',
+    undefined,
+    ReplicantMap,
+    { [x: string]: never }
+  >;
+}
