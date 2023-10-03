336b813102156503eda51e37206b2c54760dcb6a
bozitoma <bozitoma@gmail.com>
2023-10-03 22:54:41 +0900

    ♻️ スワップボタンなどのボタン類をひとつのコンポーネントにまとめた

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,12 +1,15 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import SwapHorizontalCircleRoundedIcon from '@mui/icons-material/SwapHorizontalCircleRounded';
+import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
 
 type Props = {
-  variant: 'contained' | 'outlined';
-  text: string;
-  color: 'primary' | 'error' | 'inherit' | 'secondary' | 'info' | 'success' | 'warning';
+  variant?: 'contained' | 'outlined';
+  text?: string;
+  color?: 'primary' | 'error' | 'inherit' | 'secondary' | 'info' | 'success' | 'warning';
   url?: string;
-  width: number;
+  width?: number;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
 };
 
@@ -22,6 +25,26 @@ export function Buttons({ variant, text, color, url, width, onClick }: Props) {
       >
         {text}
       </Button>
+    </>
+  );
+}
+
+export function SwapButtons() {
+  return (
+    <>
+      <IconButton color="primary">
+        <SwapHorizontalCircleRoundedIcon />
+      </IconButton>
+    </>
+  );
+}
+
+export function ClearButtons({ onClick }: Props) {
+  return (
+    <>
+      <IconButton color="primary" onClick={onClick}>
+        <RemoveCircleOutlineIcon />
+      </IconButton>
     </>
   );
 }
