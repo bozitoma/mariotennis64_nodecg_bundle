5acb60d1c3ff8526ba9a45c986821687e5aa8819
bozitoma <bozitoma@gmail.com>
2023-10-03 22:57:45 +0900

    ✨ テキストボックスのコンポーネントを作成

--- src/components/textBox.tsx
+++ src/components/textBox.tsx
@@ -1,16 +1,17 @@
-import * as React from 'react';
 import TextField, { TextFieldVariants } from '@mui/material/TextField';
 
 type Props = {
   id: string;
   label: string;
   variant: TextFieldVariants | undefined;
+  width: number;
+  size: 'small' | 'medium' | undefined;
 };
 
-export function TextBox({ id, label, variant }: Props) {
+export function TextBox({ id, label, variant, width, size }: Props) {
   return (
     <>
-      <TextField id={id} label={label} variant={variant} />
+      <TextField id={id} label={label} variant={variant} sx={{ width: { width } }} size={size} />
     </>
   );
 }
