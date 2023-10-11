35aba666bdedba20ebe5bbb9f5b28b3c32fa5794
bozitoma <bozitoma@gmail.com>
2023-10-12 07:45:30 +0900

    ⚡️ ReplicantMapを更新

--- src/types/replicant.ts
+++ src/types/replicant.ts
@@ -1,19 +1,25 @@
 // Replicantsの型を定義
 export interface ReplicantMap {
-  playerName: string;
-  gameCount: number;
+  playerName1p: string;
+  playerName2p: string;
+  gameCount1p: string; //表示する上では文字列で問題ない、numberだと型定義がめんどくさそう
+  gameCount2p: string; //表示する上では文字列で問題ない、numberだと型定義がめんどくさそう
   roundInfo: string;
   bestOfInfo: string;
   tournamentInfo: string;
-  characterInfo: string; //キャラを選択する配列があればそれを指定して良いかも
+  characterInfo1p: string; //キャラを選択する配列があればそれを指定して良いかも
+  characterInfo2p: string;
 }
 
 // Replicantsの初期値を定義
 export const replicantDefaultValues: ReplicantMap = {
-  playerName: '',
-  gameCount: 0,
+  playerName1p: '',
+  playerName2p: '',
+  gameCount1p: '0',
+  gameCount2p: '0',
   roundInfo: '',
   bestOfInfo: '',
   tournamentInfo: '',
-  characterInfo: '',
+  characterInfo1p: '',
+  characterInfo2p: '',
 };
